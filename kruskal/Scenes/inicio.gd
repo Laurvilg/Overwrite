c2cb1b6a1a0da20723657fe5260dfb8715c94c87
Valerie <avangulo@uninorte.edu.co>
2025-11-19 13:18:07 -0500

    Añadí Pantalla Inicio Kruskal

---
+++ kruskal/Scenes/inicio.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+func _on_texture_button_pressed() -> void:
+	AudioManager.SFXPlayer.stream = preload("res://inicio/audio/button-305770.mp3")
+	AudioManager.SFXPlayer.play()
+	get_tree().change_scene_to_file("res://kruskal/RebuildNet.tscn")
